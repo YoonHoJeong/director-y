94eb06b1ccd310449d8b890f34726d509e4f1157
Jeong YoonHo <uno8941@gmail.com>
2020-11-13 22:24:53 +0900

    user page edit 변경

--- accounts/static/js/edit.js
+++ accounts/static/js/edit.js
@@ -1,43 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   const reviseBtn = document.querySelector(".btn--revise");
-  const editBtns = document.querySelectorAll(".btn--edit");
   const doneBtn = document.querySelector(".btn--done");
   const inputs = document.querySelectorAll(".profile-detail-item input");
 
   const onClickRevise = (e) => {
     e.preventDefault();
-    editBtns.forEach((editBtn) => {
-      editBtn.style.display = "block";
-    });
-    doneBtn.style.display = "block";
-  };
-  const onClickDone = (e) => {
-    editBtns.forEach((editBtn) => {
-      editBtn.style.display = "none";
-    });
-    doneBtn.style.display = "none";
-    inputs.forEach((input) => {
-      input.setAttribute("readonly");
-    });
-  };
-  const onClickEdit = (e) => {
-    e.preventDefault();
-    const name = e.target.name ? e.target.name : e.target.parentNode.name;
-    let current = null;
 
+    doneBtn.style.display = "block";
+    reviseBtn.style.display = "none";
     inputs.forEach((input) => {
       if (input.name === name) {
         current = input;
       }
+      input.removeAttribute("readonly");
+      input.style.borderBottom = "1px solid black";
     });
-
-    current.removeAttribute("readonly");
-    current.style.borderBottom = "1px solid black";
   };
-
-  editBtns.forEach((editBtn) => {
-    editBtn.addEventListener("click", onClickEdit);
-  });
+  const onClickDone = (e) => {};
   reviseBtn.addEventListener("click", onClickRevise);
   doneBtn.addEventListener("click", onClickDone);
 });
